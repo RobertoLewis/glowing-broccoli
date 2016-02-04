2b26d4bceddb5e3066058692b7535e48075b085e
Roberto Lewis <roberto.lewis@gmail.com>
2016-02-03 23:25:26 -0800

    minor change

--- foote-learning_to_program/kittenbook/js/prompt.js
+++ foote-learning_to_program/kittenbook/js/prompt.js
@@ -1,2 +1,3 @@
 var userName = prompt('Hello, what\'s your name?');
-document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>';
+document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' +
+            '<p>' + projectName + ' ' + versionNumber + ' accessed on: ' + currentTime + '</p>';
