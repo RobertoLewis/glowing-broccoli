650708cf7308629cb5cb7320c93f9a019ec0c556
Roberto Lewis <roberto.lewis@gmail.com>
2016-03-02 14:08:46 -0800

    minor changes

--- foote-learning_to_program/kittenbook/js/prompt.js
+++ foote-learning_to_program/kittenbook/js/prompt.js
@@ -1,5 +1,3 @@
 var userName = prompt('Hello, what\'s your name?');
 document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' +
-            '<p>' + projectName + ' ' + versionNumber + ' accessed on: ' + currentTime + '</p>';
-
-            //test comment
+            '<p>' + kbValues.projectName + ' ' + kbValues.versionNumber + ' accessed on: ' + kbValues.currentTime + '</p>';
--- foote-learning_to_program/kittenbook/js/values.js
+++ foote-learning_to_program/kittenbook/js/values.js
@@ -1,11 +1,13 @@
-var projectName = 'kittenbook';
-var versionNumber = '0.0.1';
 var currentDate = new Date(); // Create Date object.
 
+var kbValues = {
+  projectName:'kittenbook',
+  versionNumber:'0.0.1',
   // currentTime will look like '2014-01-25 at 14:45:12'
-var currentTime = currentDate.getFullYear() + '-' + // Set year
-          (currentDate.getMonth() + 1)+ '-' + // Set month
-          currentDate.getDate() + ' at ' +   // Set day of the month
-          currentDate.getHours() + ':' +  // Set hours (military time)
-          currentDate.getMinutes() + ':' + // Set minutes
-          currentDate.getSeconds();  // Set seconds
+  currentTime: currentDate.getFullYear() + '-' + // Set year
+            (currentDate.getMonth() + 1)+ '-' + // Set month
+            currentDate.getDate() + ' at ' +   // Set day of the month
+            currentDate.getHours() + ':' +  // Set hours (military time)
+            currentDate.getMinutes() + ':' + // Set minutes
+            currentDate.getSeconds()  // Set seconds
+};
